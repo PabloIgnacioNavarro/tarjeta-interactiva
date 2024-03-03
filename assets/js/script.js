f95d060f7b34c4e2f0b85765013a0ab2df62dcad
Pablo Navarro <ignacionavarropablo@gmail.com>
2024-03-03 15:33:19 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let precioIncial = 1000;
+let cantidadInicial = 1;
+
+let precio = document.querySelector(".precio");
+let cantidad = document.querySelector(".cantidad");
+let total = document.querySelector(".total");
+
+precio.innerHTML = precioIncial;
+cantidad.innerHTML = cantidadInicial;
+total.innerHTML= precioIncial
